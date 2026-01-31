c92535f4f79282ad31c87bc57beb38aca5f41144
Daniel Rodrigues de Lima <daniel.engca@outlook.com>
2026-01-31 08:20:22 -0400

    feat: create standalone components for pets and tutores features

---
+++ src/app/modules/tutores/pages/tutor-list/tutor-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tutor-list',
+  imports: [],
+  templateUrl: './tutor-list.html',
+  styleUrl: './tutor-list.scss',
+})
+export class TutorList {
+
+}
