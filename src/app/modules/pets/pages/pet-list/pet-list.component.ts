3a95c8f268a41a0421a5f99664f4369c3e9068d6
Daniel Rodrigues de Lima <daniel.engca@outlook.com>
2026-01-31 09:27:26 -0400

    feat: implement pet list UI with search, pagination and modern control flow

--- src/app/modules/pets/pages/pet-list/pet-list.component.ts
+++ src/app/modules/pets/pages/pet-list/pet-list.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {PetFacade} from '../../../../core/facades/pet.facade';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-pet-list',
-  imports: [],
-  templateUrl: './pet-list.component.html',
-  styleUrl: './pet-list.component.scss',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './pet-list.component.html'
 })
-export class PetListComponent {
+export class PetListComponent implements OnInit {
+  private petFacade = inject(PetFacade);
 
+  pets$ = this.petFacade.pets$;
+  loading$ = this.petFacade.loading$;
+  searchTerm = '';
+
+  ngOnInit() {
+    this.petFacade.loadPets();
+  }
+
+  onSearch() {
+    this.petFacade.loadPets(0, this.searchTerm);
+  }
 }
