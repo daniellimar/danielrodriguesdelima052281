435c4996f6259653dffde399bf28c6daf5e7c684
Daniel Rodrigues de Lima <daniel.engca@outlook.com>
2026-01-31 10:28:47 -0400

    fix(auth): fix login page styling using Tailwind

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormBuilder, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
+import {AsyncPipe} from '@angular/common';
+import {AuthFacade} from '../../../../core/facades/auth.facade';
+import {BehaviorSubject} from 'rxjs';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, AsyncPipe],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
+  styleUrls: ['./login.component.scss']
 })
 export class Login {
+  private fb = inject(FormBuilder);
+  private authFacade = inject(AuthFacade);
 
+  private readonly loadingSubject = new BehaviorSubject<boolean>(false);
+  private readonly errorSubject = new BehaviorSubject<string | null>(null);
+
+  loading$ = this.loadingSubject.asObservable();
+  error$ = this.errorSubject.asObservable();
+
+  loginForm: FormGroup = this.fb.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(5)]]
+  });
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      this.loadingSubject.next(true);
+      this.errorSubject.next(null);
+
+      this.authFacade.login(this.loginForm.value).subscribe({
+        next: () => {
+          this.loadingSubject.next(false);
+        },
+        error: (err) => {
+          this.loadingSubject.next(false);
+          this.errorSubject.next(
+            err.error?.message || 'Invalid credentials. Please try again.'
+          );
+        }
+      });
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
+  get usernameControl() {
+    return this.loginForm.get('username');
+  }
+
+  get passwordControl() {
+    return this.loginForm.get('password');
+  }
 }
