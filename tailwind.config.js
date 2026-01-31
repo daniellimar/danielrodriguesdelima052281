9edc21a8531c5f17ad1ce7bdd1d3cb5b00c0a9ca
Daniel Rodrigues de Lima <daniel.engca@outlook.com>
2026-01-31 10:27:48 -0400

    feat: add Tailwind CSS globally

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#eff6ff',
+          600: '#2563eb',
+          700: '#1d4ed8',
+        }
+      }
+    }
+  },
+  plugins: []
+};
