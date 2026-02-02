5f31f21b86324c3291576f082a187fb9c7560c2f
Daniel Rodrigues de Lima <daniel.engca@outlook.com>
2026-02-01 22:28:32 -0400

    feat: add logout functionality to main navigation bar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import {Component, signal} from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import {Component, inject, signal} from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {AuthFacade} from './core/facades/auth.facade';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('pet-manager-frontend');
+
+  authFacade = inject(AuthFacade);
+
+  onLogout(): void {
+    this.authFacade.logout();
+  }
 }
