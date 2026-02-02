484c253be54748d4b13aef8349e7c0809bf3fce9
Daniel Rodrigues de Lima <daniel.engca@outlook.com>
2026-02-01 21:28:23 -0400

    chore: add routes for pet creation and editing modes

--- src/app/modules/pets/pets.routes.ts
+++ src/app/modules/pets/pets.routes.ts
@@ -1,11 +1,14 @@
 import {Routes} from '@angular/router';
 import {PetListComponent} from './pages/pet-list/pet-list.component';
 import {PetDetailComponent} from './pages/pet-detail/pet-detail.component';
+import {PetFormComponent} from './pages/pet-form/pet-form.component';
 
 export const PETS_ROUTES: Routes = [
   {
     path: '',
     component: PetListComponent
   },
-  {path: ':id', component: PetDetailComponent}
+  {path: 'novo', component: PetFormComponent},
+  {path: ':id/editar', component: PetFormComponent},
+  {path: ':id', component: PetDetailComponent},
 ];
