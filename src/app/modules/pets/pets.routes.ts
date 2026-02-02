55d488de824800d6e55e934a5cb9585ef25157e8
Daniel Rodrigues de Lima <daniel.engca@outlook.com>
2026-02-01 20:13:51 -0400

    feat: add pet detail route with component input binding support

--- src/app/modules/pets/pets.routes.ts
+++ src/app/modules/pets/pets.routes.ts
@@ -1,9 +1,11 @@
 import {Routes} from '@angular/router';
 import {PetListComponent} from './pages/pet-list/pet-list.component';
+import {PetDetailComponent} from './pages/pet-detail/pet-detail.component';
 
 export const PETS_ROUTES: Routes = [
   {
     path: '',
     component: PetListComponent
   },
+  {path: ':id', component: PetDetailComponent}
 ];
