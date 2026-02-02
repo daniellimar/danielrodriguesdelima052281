c288757eb5dcfd94e73ed10aa76a245a1289e3c1
Daniel Rodrigues de Lima <daniel.engca@outlook.com>
2026-02-01 21:52:27 -0400

    feat: implement tutor list page with search and pagination

--- src/app/modules/tutores/pages/tutor-list/tutor-list.component.ts
+++ src/app/modules/tutores/pages/tutor-list/tutor-list.component.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, inject} from '@angular/core';
+import {Router, RouterLink} from '@angular/router';
+import {AsyncPipe} from '@angular/common';
+import {TutorFacade} from '../../../../core/facades/tutor.facade';
+import {SearchBarComponent} from '../../../../shared/components/search-bar/search-bar.component';
+import {PaginationComponent} from '../../../../shared/components/pagination/pagination.component';
+import {Subject} from 'rxjs';
+import {debounceTime, distinctUntilChanged} from 'rxjs/operators';
 
 @Component({
   selector: 'app-tutor-list',
-  imports: [],
+  standalone: true,
+  imports: [AsyncPipe, RouterLink, SearchBarComponent, PaginationComponent],
   templateUrl: './tutor-list.component.html',
-  styleUrl: './tutor-list.component.scss',
+  styleUrl: './tutor-list.component.scss'
 })
-export class TutorListComponent {
+export class TutorListComponent implements OnInit {
+  private tutorFacade = inject(TutorFacade);
+  private router = inject(Router);
 
+  tutores$ = this.tutorFacade.tutores$;
+  loading$ = this.tutorFacade.loading$;
+  currentPage$ = this.tutorFacade.currentPage$;
+  totalPages$ = this.tutorFacade.totalPages$;
+
+  searchTerm = '';
+  private searchSubject = new Subject<string>();
+
+  ngOnInit(): void {
+    this.tutorFacade.loadTutores();
+
+    this.searchSubject.pipe(
+      debounceTime(400),
+      distinctUntilChanged()
+    ).subscribe(term => {
+      this.tutorFacade.loadTutores(0, term);
+    });
+  }
+
+  onSearch(term: string): void {
+    this.searchTerm = term;
+    this.searchSubject.next(term);
+  }
+
+  onPageChange(page: number): void {
+    this.tutorFacade.loadTutores(page, this.searchTerm);
+  }
+
+  onViewTutorDetails(id: number): void {
+    this.router.navigate(['/tutores', id]);
+  }
+
+  onEditTutor(id: number): void {
+    this.router.navigate(['/tutores', id, 'editar']);
+  }
 }
