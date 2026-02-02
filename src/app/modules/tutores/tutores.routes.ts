530f58aa49a8601d31e4754ad296bd31d726d18e
Daniel Rodrigues de Lima <daniel.engca@outlook.com>
2026-02-01 21:53:45 -0400

    chore: configure tutor module routes for CRUD operations

--- src/app/modules/tutores/tutores.routes.ts
+++ src/app/modules/tutores/tutores.routes.ts
@@ -1,9 +1,11 @@
 import {Routes} from '@angular/router';
 import {TutorListComponent} from './pages/tutor-list/tutor-list.component';
+import {TutorDetailComponent} from './pages/tutor-detail/tutor-detail.component';
+import {TutorFormComponent} from './pages/tutor-form/tutor-form.component';
 
 export const TUTORES_ROUTES: Routes = [
-  {
-    path: '',
-    component: TutorListComponent
-  }
+  {path: '', component: TutorListComponent},
+  {path: 'novo', component: TutorFormComponent},
+  {path: ':id/editar', component: TutorFormComponent},
+  {path: ':id', component: TutorDetailComponent}
 ];
