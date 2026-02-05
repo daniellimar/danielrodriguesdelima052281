60fcfaf4af96cc3791e5ea418d73ff722dd08f6e
Daniel Rodrigues de Lima <daniel.engca@outlook.com>
2026-02-05 18:46:52 -0400

    refactor(pets): simplify PetDetailComponent and remove unused imports

--- src/app/modules/pets/pages/pet-detail/pet-detail.component.ts
+++ src/app/modules/pets/pages/pet-detail/pet-detail.component.ts
@@ -1,10 +1,8 @@
-import {Component, OnInit, Input, inject, OnDestroy} from '@angular/core';
+import {Component, OnInit, Input, inject} from '@angular/core';
 import {AsyncPipe} from '@angular/common';
 import {RouterLink} from '@angular/router';
-import {Title} from '@angular/platform-browser';
 import {PetFacade} from '../../../../core/facades/pet.facade';
 import {DEFAULT_PET_IMAGE} from '../../../../shared/constants/default-images';
-import {tap} from 'rxjs';
 
 @Component({
   selector: 'app-pet-detail',
@@ -13,19 +11,13 @@ import {tap} from 'rxjs';
   templateUrl: './pet-detail.component.html',
   styleUrl: './pet-detail.component.scss'
 })
-export class PetDetailComponent implements OnInit, OnDestroy {
+export class PetDetailComponent implements OnInit {
   @Input() id!: string;
 
-  private readonly petFacade = inject(PetFacade);
-  private readonly titleService = inject(Title);
+  private petFacade = inject(PetFacade);
 
-  readonly pet$ = this.petFacade.selectedPet$.pipe(
-    tap(pet => {
-      if (pet) this.titleService.setTitle(`Pet: ${pet.nome}`);
-    })
-  );
-  readonly loading$ = this.petFacade.loading$;
-  readonly defaultPetImage = DEFAULT_PET_IMAGE;
+  pet$ = this.petFacade.selectedPet$;
+  loading$ = this.petFacade.loading$;
 
   ngOnInit(): void {
     if (this.id) {
@@ -33,8 +25,5 @@ export class PetDetailComponent implements OnInit, OnDestroy {
     }
   }
 
-  ngOnDestroy(): void {
-    this.petFacade.clearSelectedPet();
-    this.titleService.setTitle('Pet Manager');
-  }
+  defaultPetImage = DEFAULT_PET_IMAGE;
 }
